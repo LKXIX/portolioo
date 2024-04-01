fc8930529532e22475ae921f8cdd0a2f81e129fe
Liam Karlsson <77934642+LKXIX@users.noreply.github.com>
2024-04-01 20:23:23 +0000

    fix pdding on edu

--- src/components/Education.js
+++ src/components/Education.js
@@ -34,7 +34,7 @@ const Education = () => {
   });
 
   return (
-    <div className="my-64">
+    <div className="my-32">
       <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">Education</h2>
 
       <div ref={ref} className="relative w-[75%] mx-auto lg:w-[90%] md:w-full">
