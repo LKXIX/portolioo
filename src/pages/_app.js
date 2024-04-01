6e704dc36830fcff3c91035d130e38d7d90afda2
Liam Karlsson <karlssonliam05@gmail.com>
2024-04-01 20:23:31 +0200

    new update favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/LiamFavIcon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Add SpeedInsights component */}
       <SpeedInsights />
