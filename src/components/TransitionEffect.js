9007030e6e59f4e1139d6c0abaa6495b124858fa
Liam Karlsson <77934642+LKXIX@users.noreply.github.com>
2024-04-12 10:03:47 +0000

    fix

--- src/components/TransitionEffect.js
+++ src/components/TransitionEffect.js
@@ -5,7 +5,7 @@ const TransitionEffect = () => {
   return (
     <>
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-dark"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-secondary dark:bg-light"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x:"0%", width:"0%" }}
         exit={{x:["0%", "100%"], width:["0%","100%"]}}
@@ -18,7 +18,7 @@ const TransitionEffect = () => {
         transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
       />
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-light dark:bg-light"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark dark:bg-light"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "0%" }}
         transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
