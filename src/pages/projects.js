a5d1613e820fc5133aff88aa691db09e93b560e0
Liam Karlsson <77934642+LKXIX@users.noreply.github.com>
2024-04-23 12:21:33 +0000

    fix malm uf

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import proj1 from "../../public/images/projects/Malm UFf.jpg";
+import proj1 from "../../public/images/projects/Malm.jpg";
 import proj2 from "../../public/images/projects/forex.jpg";
 /* import proj3 from "../../public/images/projects/fashion-studio-website.jpg";
 import proj4 from "../../public/images/projects/portfolio-cover-image.jpg";
