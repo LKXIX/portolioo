f99824377f25654fdc9fca24e9fe850d39a686ac
Liam Karlsson <77934642+LKXIX@users.noreply.github.com>
2024-10-05 13:51:48 +0000

    fix new picture in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
-import profile from "../../public/images/profile/liam.jpg";
+import profile from "../../public/images/profile/liam2.jpg";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 import { useEffect, useRef } from "react";
 import Skills from "@/components/Skills";
