a67cc8d12da722e25ef40f8adc0fc52fa00194b0
Liam Karlsson <77934642+LKXIX@users.noreply.github.com>
2024-10-29 20:19:04 +0000

    added

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -5,26 +5,33 @@ export async function getServerSideProps({ res }) {
   
     // Define the XML sitemap content
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>https://acme.com</loc>
-      <lastmod>2023-04-06T15:02:24.021Z</lastmod>
-      <changefreq>yearly</changefreq>
-      <priority>1</priority>
-    </url>
-    <url>
-      <loc>https://acme.com/about</loc>
-      <lastmod>2023-04-06T15:02:24.021Z</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>0.8</priority>
-    </url>
-    <url>
-      <loc>https://acme.com/blog</loc>
-      <lastmod>2023-04-06T15:02:24.021Z</lastmod>
-      <changefreq>weekly</changefreq>
-      <priority>0.5</priority>
-    </url>
-  </urlset>`;
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://liamkarlsson.com</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>yearly</changefreq>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>https://liamkarlsson.com/about</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>https://liamkarlsson.com/projects</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.5</priority>
+      </url>
+      <url>
+        <loc>https://liamkarlsson.com/articles</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+      </url>
+      <!-- Add more URLs as needed -->
+    </urlset>`;
   
     // Send the response
     res.write(sitemap);
@@ -33,9 +40,8 @@ export async function getServerSideProps({ res }) {
     return {
       props: {},
     };
-  }
+}
   
-  export default function Sitemap() {
+export default function Sitemap() {
     return null;
-  }
-  
+}
