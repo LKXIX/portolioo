487e35cca1e2a583963d8999caf9189ca2f2834f
Liam Karlsson <77934642+LKXIX@users.noreply.github.com>
2024-11-01 20:29:14 +0000

    fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -101,15 +101,14 @@ export default function About() {
         dark:bg-light  "
               />
               <Image
-  className="h-auto w-full rounded-2xl hidden md:block"
-  src={profile}
-  alt="Liam Karlsson"
-  sizes="(max-width: 768px) 50vw,
-        (max-width: 1200px) 50vw,
-        33vw"
-  priority
-/>
-
+                className="h-auto w-full rounded-2xl"
+                src={profile}
+                alt="Liam Karlsson"
+                sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                priority
+              />
             </div>
             <div className="col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row 
             xl:items-center md:order-3">
